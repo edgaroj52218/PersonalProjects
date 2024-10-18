679c44f08ea9d6f8bbc7e377b2ea91092c6c40f0
Steven <eso@njit.edu>
2024-10-18 18:34:33 -0400

    added Java files for heroes and .gitignore for class

---
+++ DamageHeroes.java
@@ -0,0 +1,29 @@
+import java.util.ArrayList;
+
+public class DamageHeroes {
+    public DamageHeroes() {
+    ArrayList<String> dpsheroes = new ArrayList<String>();
+    //name of all current dps heroes
+    dpsheroes.add("Ashe");
+    dpsheroes.add("Bastion");
+    dpsheroes.add("Cassidy");
+    dpsheroes.add("Echo");
+    dpsheroes.add("Genji");
+    dpsheroes.add("Hanzo");
+    dpsheroes.add("Junkrat");
+    dpsheroes.add("Mei");
+    dpsheroes.add("Pharah");
+    dpsheroes.add("Reaper");
+    dpsheroes.add("Sojourn");
+    dpsheroes.add("Soldier: 76");
+    dpsheroes.add("Sombra");
+    dpsheroes.add("Symmetra");
+    dpsheroes.add("Torbjorn");
+    dpsheroes.add("Tracer");
+    dpsheroes.add("Venture");
+    dpsheroes.add("Widowmaker");
+
+    //print all heroes
+    System.out.println(dpsheroes);
+    }
+}
---
+++ SupportHeroes.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+
+public class SupportHeroes {
+    public SupportHeroes() {
+        ArrayList<String> supportHeroes = new ArrayList<String>();
+        //list of current support heroes
+        supportHeroes.add("Ana");
+        supportHeroes.add("Baptiste");
+        supportHeroes.add("Brigitte");
+        supportHeroes.add("Illari");
+        supportHeroes.add("Juno");
+        supportHeroes.add("Kiriko");
+        supportHeroes.add("Lifeweaver");
+        supportHeroes.add("Lucio");
+        supportHeroes.add("Mercy");
+        supportHeroes.add("Moira");
+        supportHeroes.add("Zenyatta");
+
+        //print all support heroes
+        System.out.println(supportHeroes);
+    }    
+}
